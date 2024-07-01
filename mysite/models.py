d455aa79a60540c05171057d955a49168774d152
narbaev2024 <aziznarbaev2020@outlook.com>
2024-07-01 17:14:29 +0600

    new files

---
+++ mysite/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=32)
+    descriptionm = models.TextField()
+
+
+
+class Professor(models.Model):
+    user = models.CharField(max_length=32, unique=True)
+    department = models.TextField()
+    title = models.ForeignKey(Department, on_delete=models.CASCADE)
+    CHOICES_BIO = (
+        ("", ""),
+        ("", ""),
+        ("", "")
+    )
+    bio = models.CharField(max_length=32, choices=CHOICES_BIO)
+
+
+class Student(models.Model):
+    user = models.CharField(max_length=32, unique=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    enrollment_date = models.DateTimeField()
+    graduation_date = models.DateTimeField()
+
+
+class Cours(models.Model):
+    name = models.CharField(max_length=32)
+    code = models.PositiveSmallIntegerField()
+    description = models.TextField()
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    price = models.PositiveSmallIntegerField()
