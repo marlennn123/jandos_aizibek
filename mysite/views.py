48c48555d2eb9af24d6d738e5be8427f4514ab79
narbaev2024 <165474675+narbaev2024@users.noreply.github.com>
2024-07-02 19:13:49 +0500

    Update views.py

--- mysite/views.py
+++ mysite/views.py
@@ -1,26 +1,35 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets,permissions
 from django_filters.rest_framework import DjangoFilterBackend
-from .models import Department , Professor, Student, Cours
-from .serializers import DeaprtmentSerializer, ProfessorSerializer, StudentSerializer, CourseSerializer
+from .models import Department, Professor, Student, Cours
+from .serializers import DepartmentSerialzer, ProfessorSerialzer, StudentSerialzer, CoursSerialzer
 from rest_framework.filters import SearchFilter
-
-
-class DepartmentViewSet(viewsets.ModelViewSet):
-    queryset = Department.objects.all()
-    serializer_class = DeaprtmentSerializer
-
-class ProfessorViewSet(viewsets.ModelViewSet):
-    queryset = Professor.objects.all()
-    serializer_class = ProfessorSerializer
-
-class StudentViewSet(viewsets.ModelViewSet):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
+from .filters import  ProfessorFilter,  StudentFilter, CoursFilter
 
 class CoursViewSet(viewsets.ModelViewSet):
     queryset = Cours.objects.all()
-    serializer_class = CourseSerializer
+    serializer_class = CoursSerialzer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_class = CoursFilter
+    search_fields = ['name']
+    permission_classes = [permissions.IsAdminUser]
 
+class ProfessorViewSet(viewsets.ModelViewSet):
+    queryset = Professor.objects.all()
+    serializer_class = ProfessorSerialzer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_class = ProfessorFilter
+    search_fields = ['user']
+    permission_classes = [permissions.IsAdminUser]
 
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerialzer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_class = StudentFilter
+    search_fields = ['user']
+    permission_classes = [permissions.IsAdminUser]
 
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerialzer
 
