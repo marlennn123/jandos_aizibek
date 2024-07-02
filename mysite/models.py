7f12fea973d8652d9f10e7bb695def861dd46637
narbaev2024 <165474675+narbaev2024@users.noreply.github.com>
2024-07-02 19:14:54 +0500

    Update models.py

--- mysite/models.py
+++ mysite/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 
-
 class Department(models.Model):
     name = models.CharField(max_length=32)
-    descriptionm = models.TextField()
+    description = models.TextField()
 
+    def __str__(self):
+        return self.name
 
 class Professor(models.Model):
     user = models.CharField(max_length=32, unique=True)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    title = models.CharField(max_length=32)
     bio = models.TextField()
 
+    def __str__(self):
+        return self.user
 
 class Student(models.Model):
     user = models.CharField(max_length=32, unique=True)
@@ -18,11 +22,15 @@ class Student(models.Model):
     enrollment_date = models.DateTimeField()
     graduation_date = models.DateTimeField()
 
+    def __str__(self):
+        return self.user
 
 class Cours(models.Model):
     name = models.CharField(max_length=32)
-    code = models.PositiveSmallIntegerField()
+    code = models.IntegerField()
     description = models.TextField()
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
     price = models.PositiveSmallIntegerField()
+    def __str__(self):
+        return self.name
